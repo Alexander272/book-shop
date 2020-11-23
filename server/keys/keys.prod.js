307ea9672ece3e20c402ceda05d36ad91a2c1e24
Alexander <martynovalexander272@gmail.com>
2020-11-23 15:21:30 +0500

    stupid decision

--- server/keys/keys.prod.js
+++ server/keys/keys.prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGODB_URL: process.env.MONGODB_URL,
-    SESSION_SECRET: process.env.SESSION_SECRET,
-    BASE_URL: process.env.BASE_URL,
+    MONGODB_URL: 'mongodb+srv://Alex:2fQd48Ea5if7KRgL@cluster0-gexu4.mongodb.net/bookShop',
+    SESSION_SECRET: 'some secret value',
+    BASE_URL: 'https://book-shop-iota.vercel.app',
 }
